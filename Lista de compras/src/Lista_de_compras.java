36dbb70663fe9b3ae68315b6245fafe8d362f4dd
Slimow <81116971+Slimow@users.noreply.github.com>
2021-03-21 18:54:02 -0300

    Update Lista_de_compras.java

--- Lista de compras/src/Lista_de_compras.java
+++ Lista de compras/src/Lista_de_compras.java
@@ -1,38 +1,26 @@
 package lista_de_compras;
+
 public class Lista_de_compras {
 private String lugarCompras;
 
 
 public Lista_de_compras(String lc) {
-	lugarCompras = lc;
-	
-	
-}
-public class local {
-	public String mercado;
-	public String acougue;
-	public String feira;
-	public String emporio;
-	
-	public Local(String m, String a, String f, String e) {
-		mercado = m;
-		acougue = a;
-		feira = f;
-		emporio = e;
-	}
+    lugarCompras = lc;
+
+
 }
 public void escolher () {
-	
+
 }
 
 
 public String getLugarCompras() {
-	return lugarCompras;
+    return lugarCompras;
 }
 
 
 public void setLugarCompras(String lugarCompras) {
-	this.lugarCompras = lugarCompras;
+    this.lugarCompras = lugarCompras;
 }
 
 }
